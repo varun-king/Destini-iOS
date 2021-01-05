9de2f9a3f12701f1f0abc97e8d1e93f0bc6f88e9
Varun Patel <varunpatel95.canada@gmail.com>
2021-01-05 17:28:44 -0500

    Initial Commit

---
+++ Destini-iOS Manual/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Destini-iOS Manual
+//
+//  Created by apple on 1/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
